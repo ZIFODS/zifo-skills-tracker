c882211c307fb7b02a2a6961d7dcd1dba63f0db6
Joseph Smith <joe.smith@zifornd.com>
2022-09-26 15:20:36 +0100

    Convert neo4j json file to d3 json

--- pipeline/src/js_skills.py
+++ pipeline/src/js_skills.py
@@ -2,6 +2,9 @@
 Convert existing skills.json to d3.js graph compatible form
 """
 
+import json
+
+
 def neo4j_to_d3_json(neo4j_json):
 
     d3_json = {"nodes": [], "links": []}
@@ -26,3 +29,14 @@ def neo4j_to_d3_json(neo4j_json):
             )
 
     return d3_json
+
+if __name__ == "__main__":
+    with open("pipeline/src/neo4j_skills.json", "r") as neo4j_file:
+        neo4j_json = json.load(neo4j_file)
+
+        d3_json = neo4j_to_d3_json(neo4j_json)
+
+        with open("frontend/src/data/d3_skills.json", "w+") as d3_file:
+            json.dump(d3_json, d3_file, indent=4)
+
+        
