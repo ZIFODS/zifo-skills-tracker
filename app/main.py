f7bc2cbdc92b428725d6b88528e80b729f412569
jsmith-zifo <joe.smith@zifornd.com>
2022-09-30 10:28:10 +0100

    Health check for API

--- app/main.py
+++ app/main.py
@@ -5,3 +5,8 @@ from app.routers.skills import skills_router
 app = FastAPI()
 
 app.include_router(skills_router, prefix="/skills", tags=["Skills"])
+
+@app.get("/health", tags=["health_check"])
+def health_check():
+    """Health check"""
+    return {"message": "Server is healthy"}
