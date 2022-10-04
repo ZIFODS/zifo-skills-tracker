455088556c28899371c893078c5565a9a7206716
jsmith-zifo <joe.smith@zifornd.com>
2022-10-04 16:02:05 +0100

    Base redux state

---
+++ frontend/src/app/rootSaga.ts
@@ -0,0 +1,9 @@
+import { all, fork } from "redux-saga/effects";
+import watchGraphSaga from "../components/graph/graphSaga";
+
+export default function* rootSaga() {
+  const sagas = [
+    watchGraphSaga
+  ];
+  yield all(sagas.map((saga) => fork(saga)));
+}
