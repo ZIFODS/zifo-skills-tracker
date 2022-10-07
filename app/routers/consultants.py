f535f90579ba8656c5af4989e2706ae2677cc34f
jsmith-zifo <joe.smith@zifornd.com>
2022-10-07 14:03:33 +0100

    Allow multiple AND queries

--- app/routers/consultants.py
+++ app/routers/consultants.py
@@ -1,24 +1,38 @@
 from typing import List
 from fastapi import APIRouter, Query
-from app.utils import NEO4J_TO_D3_CYPHER
+from app.utils import neo4j_to_d3_cypher
 
 from pipeline.src.neo4j_connect import Neo4jConnection
 
 consultants_router = APIRouter()
 
+# MATCH pa=(c:Consultant)-[:KNOWS]-(s) where s.Name = 'REACT' 
+# unwind nodes(pa) as na
+# MATCH pb=(na)-[:KNOWS]->(t) where t.Name = "REDUX"
+# unwind nodes(pb) as nb unwind relationships(pb) as rb
+# with collect( distinct {id: ID(nb), name: nb.Name, group: labels(nb)[0]}) as nl, 
+# collect( distinct {id: ID(rb), source: ID(startnode(rb)), target: ID(endnode(rb))}) as rl
+# RETURN {nodes: nl, links: rl}
+
+def char(num: int):
+    return chr(num + 97)
+
 @consultants_router.get("/", name="Filter by skills")
 async def filter_consultants_by_skills(skills: List[str] = Query(default=...)):
     conn = Neo4jConnection(uri="neo4j://neo4j-db:7687", user="neo4j", password="test")
 
-    query = "MATCH p=(c:Consultant)-[:KNOWS]->(s) "
+    query = "MATCH pa=(c:Consultant)-[:KNOWS]->(sa) "
     for i, skill in enumerate(skills):
         if i == 0:
-            query += "where "
-        query += f"s.Name = '{skill}'"
-        if i != len(skills) - 1:
-            query += " AND "
+            query += f"where s{char(i)}.Name = '{skill}'"
 
-    query += NEO4J_TO_D3_CYPHER
+        else:
+            query += f" unwind nodes(p{char(i-1)}) as n{char(i-1)}"
+            query += f" MATCH p{char(i)}=(n{char(i-1)})-[:KNOWS]->(s{char(i)}) where s{char(i)}.Name = '{skill}'"
+
+    query += neo4j_to_d3_cypher(char(i))
+
+    print(query)
 
     result = conn.query(query)
     
--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,13 @@
-NEO4J_TO_D3_CYPHER = '''unwind nodes(p) as n unwind relationships(p) as r
-                        with collect( distinct {id: ID(n), name: n.Name, group: labels(n)[0]}) as nl, 
-                        collect( distinct {id: ID(r), source: ID(startnode(r)), target: ID(endnode(r))}) as rl
-                        RETURN {nodes: nl, links: rl}
-                    '''
+def neo4j_to_d3_cypher(final_char):
+    query = f"unwind nodes(p{final_char}) as n{final_char} unwind relationships(p{final_char}) as r{final_char}"
+
+    query += " with collect( distinct {"
+    query += f"id: ID(n{final_char}), name: n{final_char}.Name, group: labels(n{final_char})[0]"
+    query += "}) as nzz,"
+
+    query += " collect( distinct {"
+    query += f"id: ID(r{final_char}), source: ID(startnode(r{final_char})), target: ID(endnode(r{final_char}))"
+    query += "}) as rzz"
+    
+    query += " RETURN {nodes: nzz, links: rzz}"
+    return query
