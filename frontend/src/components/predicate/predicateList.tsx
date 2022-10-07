137786efb681508a7ea37cebff7cd8d5e2c3f2df
jsmith-zifo <joe.smith@zifornd.com>
2022-10-07 17:38:54 +0100

    Align d3 colours with predicate colours

--- frontend/src/components/predicate/predicateList.tsx
+++ frontend/src/components/predicate/predicateList.tsx
@@ -2,22 +2,11 @@ import React from "react"
 import { Stack, Typography, Paper } from "@mui/material";
 import { useAppSelector } from "../../app/hooks";
 import { selectPredicateList } from "./predicateSlice";
+import { selectAllNodes } from "../graph/graphSlice";
+import { getUniqueGroups } from "../../hooks/useD3";
+import * as d3 from "d3"
 
 export default function PredicateList() {
-    const colorGroupLinks: Record<string, string> = {
-        Consultant: "#cbe4f9",
-        ScienceApps: "#eff9da",
-        Services: "#f9d8d6",
-        Methodologies: "#cdf5f6",
-        Process: "#d6cdea",
-        Other_Products: "#cdf5f6",
-        Regulatory: "#f9ebdf",
-        Data_Management: "#d6cdea",
-        Languages: "#cbe4f9",
-        programming: "#eff9da",
-        Miscellaneous: "#f9d8d6",
-        Infrastructure: "#f9ebdf",
-    }
     const groupDisplayNameLinks: Record<string, string> = {
         Consultant: "Consultant",
         ScienceApps: "Science apps",
@@ -34,6 +23,8 @@ export default function PredicateList() {
     }
 
     const predicateList = useAppSelector(selectPredicateList)
+    const nodeData = useAppSelector(selectAllNodes)
+    const groups = getUniqueGroups(nodeData)
 
     return(
         <Stack spacing={2}>
@@ -41,7 +32,7 @@ export default function PredicateList() {
                 return(
                     <Paper>
                       <Stack direction="row" alignItems="center">
-                          <Typography sx={{backgroundColor: colorGroupLinks[pred.group], px:1.5, py:1, fontSize: 14}}>{groupDisplayNameLinks[pred.group]}</Typography>
+                          <Typography sx={{backgroundColor: d3.schemePaired[groups.indexOf(pred.group)] + "90", px:1.5, py:1, fontSize: 14}}>{groupDisplayNameLinks[pred.group]}</Typography>
                           <Typography sx={{pl:2, fontSize: 14}}>{pred.name}</Typography>
                       </Stack>
                     </Paper>
