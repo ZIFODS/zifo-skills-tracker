ccb22666124a80377fda564129c18dc5de3e8323
jsmith-zifo <joe.smith@zifornd.com>
2022-10-07 09:36:58 +0100

    Convert neo4j output to d3 json

--- app/routers/consultants.py
+++ app/routers/consultants.py
@@ -1,5 +1,6 @@
 from typing import List
 from fastapi import APIRouter, Query
+from app.utils import NEO4J_TO_D3_CYPHER
 
 from pipeline.src.neo4j_connect import Neo4jConnection
 
@@ -9,7 +10,7 @@ consultants_router = APIRouter()
 async def filter_consultants_by_skills(skills: List[str] = Query(default=...)):
     conn = Neo4jConnection(uri="neo4j://neo4j-db:7687", user="neo4j", password="test")
 
-    query = "MATCH (c:Consultant)-[:KNOWS]->(s) "
+    query = "MATCH p=(c:Consultant)-[:KNOWS]->(s) "
     for i, skill in enumerate(skills):
         if i == 0:
             query += "where "
@@ -17,12 +18,10 @@ async def filter_consultants_by_skills(skills: List[str] = Query(default=...)):
         if i != len(skills) - 1:
             query += " AND "
 
-    query += " return c,s"
-
-    print(query)
+    query += NEO4J_TO_D3_CYPHER
 
     result = conn.query(query)
     
     conn.close()
 
-    return result
+    return result[0][0]
