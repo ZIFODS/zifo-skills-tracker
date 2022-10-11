b20e9c83a820c17e8b41f60c59a672d2fb9c461a
jsmith-zifo <joe.smith@zifornd.com>
2022-10-11 17:49:11 +0100

    Add bitwise operator buttons

---
+++ frontend/src/components/search/bitwiseOperators.tsx
@@ -0,0 +1,59 @@
+import React from "react"
+import { Button, Stack, ToggleButtonGroup, ToggleButton } from "@mui/material"
+import { useAppDispatch, useAppSelector } from "../../app/hooks"
+import { clearSearchList, selectSearchList } from "./searchSlice"
+import { getGraphDataRequest } from "../graph/graphSlice"
+import { useEffect } from "react"
+
+export default function BitwiseOperators() {
+
+  const dispatch = useAppDispatch()
+
+  const searchList = useAppSelector(selectSearchList)
+
+  const [operator, setOperator] = React.useState<string | null>("AND")
+
+  const handleOperatorChange = (
+    _event: React.MouseEvent<HTMLElement>,
+    newOperator: string | null,
+  )  => {
+    setOperator(newOperator);
+  }
+
+  const [parentheses, setParentheses] = React.useState<string | null>("")
+
+  const handleParenthesesChange = (
+    _event: React.MouseEvent<HTMLElement>,
+    newParentheses: string | null,
+  )  => {
+    setParentheses(newParentheses);
+  }
+
+  return(
+    <Stack direction="row" spacing={2}>
+      <ToggleButtonGroup
+        value={operator}
+        exclusive
+        onChange={handleOperatorChange}
+      >
+        <ToggleButton value="AND">
+          AND
+        </ToggleButton>
+        <ToggleButton value="OR">
+          OR
+        </ToggleButton>
+      </ToggleButtonGroup>
+      <ToggleButtonGroup
+        value={parentheses}
+        onChange={handleParenthesesChange}
+      >
+        <ToggleButton value="[">
+          [
+        </ToggleButton>
+        <ToggleButton value="]">
+          ]
+        </ToggleButton>
+      </ToggleButtonGroup>
+    </Stack>
+  )
+}
