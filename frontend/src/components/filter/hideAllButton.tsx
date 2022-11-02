592e1eec846093296f43af4b40119f2bdb15202e
jsmith-zifo <joe.smith@zifornd.com>
2022-11-02 14:16:59 +0000

    Run prettier on filter section

--- frontend/src/components/filter/hideAllButton.tsx
+++ frontend/src/components/filter/hideAllButton.tsx
@@ -1,29 +1,46 @@
-import React from "react"
+import React from "react";
 import { Button } from "@mui/material";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
-import { filterGraphDataRequest, isGraphFilled, setHiddenGroups, selectAllNodes, selectCurrentSearchedList } from "../graph/graphSlice";
+import {
+  filterGraphDataRequest,
+  isGraphFilled,
+  setHiddenGroups,
+  selectAllNodes,
+  selectCurrentSearchedList,
+} from "../graph/graphSlice";
 import { getUniqueGroups } from "../../hooks/useD3";
 
 export default function HideAllButton() {
+  const graphFilled = useAppSelector(isGraphFilled);
 
-   const graphFilled = useAppSelector(isGraphFilled) 
+  const dispatch = useAppDispatch();
 
-  const dispatch = useAppDispatch()
+  const allNodeData = useAppSelector(selectAllNodes);
+  const allGroups = getUniqueGroups(allNodeData);
 
-  const allNodeData = useAppSelector(selectAllNodes)
-  const allGroups = getUniqueGroups(allNodeData)
-
-  let skills = useAppSelector(selectCurrentSearchedList)
+  let skills = useAppSelector(selectCurrentSearchedList);
 
   const handleClick = () => {
-    const allSkillGroups = allGroups.filter(function(group: string) {return group !== "Consultant"})
-    allSkillGroups.map(function(group: string) {dispatch(setHiddenGroups(group))})
-    skills.length && dispatch(filterGraphDataRequest({skills: skills, hiddenGroups: allSkillGroups}))
-    }
+    const allSkillGroups = allGroups.filter(function (group: string) {
+      return group !== "Consultant";
+    });
+    allSkillGroups.map(function (group: string) {
+      dispatch(setHiddenGroups(group));
+    });
+    skills.length &&
+      dispatch(
+        filterGraphDataRequest({ skills: skills, hiddenGroups: allSkillGroups })
+      );
+  };
 
-  return(
-    <Button variant="outlined" disabled={!graphFilled} sx={{p:0.5, fontSize: 10}} onClick={handleClick}>
-        Hide all
+  return (
+    <Button
+      variant="outlined"
+      disabled={!graphFilled}
+      sx={{ p: 0.5, fontSize: 10 }}
+      onClick={handleClick}
+    >
+      Hide all
     </Button>
-  )
+  );
 }
--- frontend/src/components/filter/showAllButton.tsx
+++ frontend/src/components/filter/showAllButton.tsx
@@ -1,24 +1,34 @@
-import React from "react"
+import React from "react";
 import { Button } from "@mui/material";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
-import { filterGraphDataRequest, clearHiddenGroups, isGraphFilled, selectCurrentSearchedList } from "../graph/graphSlice";
+import {
+  filterGraphDataRequest,
+  clearHiddenGroups,
+  isGraphFilled,
+  selectCurrentSearchedList,
+} from "../graph/graphSlice";
 
 export default function ShowAllButton() {
+  const graphFilled = useAppSelector(isGraphFilled);
 
-   const graphFilled = useAppSelector(isGraphFilled) 
+  const dispatch = useAppDispatch();
 
-  const dispatch = useAppDispatch()
-
-  let skills = useAppSelector(selectCurrentSearchedList)
+  let skills = useAppSelector(selectCurrentSearchedList);
 
   const handleClick = () => {
-    dispatch(clearHiddenGroups())
-    skills.length && dispatch(filterGraphDataRequest({skills: skills, hiddenGroups: []}))
-    }
+    dispatch(clearHiddenGroups());
+    skills.length &&
+      dispatch(filterGraphDataRequest({ skills: skills, hiddenGroups: [] }));
+  };
 
-  return(
-    <Button variant="outlined" disabled={!graphFilled} sx={{p:0.5, fontSize: 10}} onClick={handleClick}>
-        Show all
+  return (
+    <Button
+      variant="outlined"
+      disabled={!graphFilled}
+      sx={{ p: 0.5, fontSize: 10 }}
+      onClick={handleClick}
+    >
+      Show all
     </Button>
-  )
+  );
 }
