500c1bdd3436dfa2389ca4b119ca18ea6818cb8b
jsmith-zifo <joe.smith@zifornd.com>
2022-11-21 17:53:31 +0000

    Update landing display message

--- frontend/src/components/landingDisplay.tsx
+++ frontend/src/components/landingDisplay.tsx
@@ -8,7 +8,7 @@ export default function LandingDisplay() {
   return (
     <Stack spacing={5} alignItems="center">
       <Typography variant="h4" sx={{ color: "#808080" }}>
-        Search with a set of skills to visualise Consultants
+        Perform a search to visualise Consultants and their skills
       </Typography>
       <img src={require("../images/zifo-logo.png")} width="150" height="75" />
     </Stack>
