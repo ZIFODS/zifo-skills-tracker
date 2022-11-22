e5c3a2343a4c157cbd66484ffe3f3a42c795efca
jsmith-zifo <joe.smith@zifornd.com>
2022-11-22 11:19:29 +0000

    Central align and pad landing display text

--- frontend/src/components/landingDisplay.tsx
+++ frontend/src/components/landingDisplay.tsx
@@ -7,7 +7,7 @@ import { Stack, Typography } from "@mui/material";
 export default function LandingDisplay() {
   return (
     <Stack spacing={5} alignItems="center">
-      <Typography variant="h4" sx={{ color: "#808080" }}>
+      <Typography variant="h4" align="center" sx={{ color: "#808080", px: 10 }}>
         Perform a search to visualise Consultants and their skills
       </Typography>
       <img src={require("../images/zifo-logo.png")} width="150" height="75" />
