59d95225a56377ce14f88aa822494855ff29a7c2
MatyasJ <matyas.juhasz@web.de>
2023-04-21 14:35:46 +0200

    Removed backslashes from test_docker.sh

--- test_docker.sh
+++ test_docker.sh
@@ -1,4 +1,4 @@
-docker compose -f docker/docker-compose.dev.yml down -v; \
-docker compose -f docker/docker-compose.dev.yml up -d --build; \
-docker compose -f docker/docker-compose.dev.yml exec -it python-api poetry run python tests/utils/load_mock_data.py; \
-docker compose -f docker/docker-compose.dev.yml exec -it python-api poetry run python -m pytest -v --cov=app --cov-report=xml --cov-report=term-missing; \
+docker compose -f docker/docker-compose.dev.yml down -v;
+docker compose -f docker/docker-compose.dev.yml up -d --build;
+docker compose -f docker/docker-compose.dev.yml exec -it python-api poetry run python tests/utils/load_mock_data.py;
+docker compose -f docker/docker-compose.dev.yml exec -it python-api poetry run python -m pytest -v --cov=app --cov-report=xml --cov-report=term-missing;
