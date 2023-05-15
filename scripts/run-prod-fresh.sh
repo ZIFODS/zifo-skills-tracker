9de369e649f1c7a8025c1bb4eeefe03ecfccf4a1
joekitsmith <joe.smith@zifornd.com>
2023-05-15 10:38:06 +0100

    Spin prod containers down before launch

--- scripts/run-prod-fresh.sh
+++ scripts/run-prod-fresh.sh
@@ -1,2 +1,3 @@
+docker compose -f docker/docker-compose.prod.yml down;
 bash scripts/init-letsencrypt.sh;
 docker compose -f docker/docker-compose.prod.yml up -d --build;
--- scripts/run-prod.sh
+++ scripts/run-prod.sh
@@ -1,1 +1,2 @@
+docker compose -f docker/docker-compose.prod.yml down;
 docker compose -f docker/docker-compose.prod.yml up -d --build;
