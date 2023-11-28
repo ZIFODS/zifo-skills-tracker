c3073caecff71239cf19d8bd294a6b5bc0f22679
joekitsmith <joesmith29@hotmail.co.uk>
2023-11-28 17:23:35 +0000

    Combine build and push step

--- scripts/build-push-prod.sh
+++ scripts/build-push-prod.sh
@@ -7,10 +7,16 @@ AWS_REGION="eu-west-2"
 ECR_REPO_NAME_FRONTEND="skills-tracker-frontend"
 ECR_REPO_URI_FRONTEND="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPO_NAME_FRONTEND"
 
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPO_NAME_FRONTEND
+
 docker build -t "$ECR_REPO_URI_FRONTEND:latest" -f docker/frontend/Dockerfile .
+docker push "$ECR_REPO_URI_FRONTEND:latest"
 
 # API
 ECR_REPO_NAME_API="skills-tracker-api"
 ECR_REPO_URI_API="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPO_NAME_API"
 
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPO_NAME_API
+
 docker build -t "$ECR_REPO_URI_API:latest" -f docker/api/Dockerfile .
+docker push "$ECR_REPO_URI_API:latest"
