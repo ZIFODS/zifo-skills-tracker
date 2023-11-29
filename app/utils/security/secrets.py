bc7d6fa916096167f5d5fc091a9ee58662343f82
joekitsmith <joesmith29@hotmail.co.uk>
2023-11-29 18:54:49 +0000

    Test containers work before pushing

--- app/utils/security/secrets.py
+++ app/utils/security/secrets.py
@@ -1,10 +1,12 @@
 import json
+import logging
 
 import boto3
 
 
 def get_secret(secret_name: str, aws_region: str, aws_kms_key: str):
     # Create a Secrets Manager client
+    logging.info(secret_name)
     session = boto3.session.Session()
     client = session.client(service_name="secretsmanager", region_name=aws_region)
 
