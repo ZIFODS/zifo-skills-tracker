90d423438c9d9d6604d79420898ea53f1a8f49e3
joekitsmith <joesmith29@hotmail.co.uk>
2023-11-29 19:21:08 +0000

    Fix secrets issues

--- app/utils/security/secrets.py
+++ app/utils/security/secrets.py
@@ -3,13 +3,14 @@ import logging
 
 import boto3
 
+logger = logging.Logger("logger")
 
-def get_secret(secret_name: str, aws_region: str, aws_kms_key: str):
+
+def get_secret(secret_name: str, aws_region: str):
     # Create a Secrets Manager client
-    logging.info(secret_name)
     session = boto3.session.Session()
     client = session.client(service_name="secretsmanager", region_name=aws_region)
 
     get_secret_value_response = client.get_secret_value(SecretId=secret_name)
 
-    return json.loads(get_secret_value_response[aws_kms_key])
+    return json.loads(get_secret_value_response["SecretString"])
