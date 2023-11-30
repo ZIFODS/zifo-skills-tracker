28e5423c8556ded82bd38017c27da3a3a18fffb2
joekitsmith <joesmith29@hotmail.co.uk>
2023-11-30 20:43:48 +0000

    Use public IP

--- terraform/main.tf
+++ terraform/main.tf
@@ -202,5 +202,5 @@ resource "aws_route53_record" "skills_tracker_record" {
   name    = "skills.zifo-tech.com"
   type    = "A"
   ttl     = "300"
-  records = [aws_eip.skills_tracker_public_ip]
+  records = [aws_eip.skills_tracker_public_ip.public_ip]
 }
